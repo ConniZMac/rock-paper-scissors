1f96ec46046878f7ed4b16f5e843db39f8005cef
ConniZMac <constanza.macnamara@gmail.com>
2022-12-20 15:22:27 -0300

    primeras variables

---
+++ app.js
@@ -0,0 +1,9 @@
+let userScore = 0;
+let pcScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("pc-score");
+const scoreBoard = document.querySelector("score_board");
+const result = document.querySelector("result > p");
+const rock = document.getElementById("r");
+const paper = document.getElementById("p");
+const scissors = document.getElementById("s");

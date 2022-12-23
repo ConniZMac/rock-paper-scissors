c8ef842e06b9ffb253d53b3efc28ca416327b211
ConniZMac <constanza.macnamara@gmail.com>
2022-12-22 23:30:08 -0300

    Funcion aleatorio

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ const result = document.querySelector("result > p");
 const rock = document.getElementById("r");
 const paper = document.getElementById("p");
 const scissors = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ["r", "p", "s"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+  }
+  
+  function convertToWord(letter) {
+    switch (letter) {
+      case "r":
+        return "Rock";
+      case "p":
+          return "Paper";
+      case "s":
+          return "Scissors";
+    }
+  }
